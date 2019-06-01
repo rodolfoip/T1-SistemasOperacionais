05094e642e9dbade43fefe3788d81a4a92ca2e17
Rodolfo <rodolfo.pereirailce@gmail.com>
2019-06-01 00:39:54 -0300

    Rodando com pthreads

--- jogo.c
+++ jogo.c
@@ -1,33 +1,40 @@
 #include <curses.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
 
-#define EMPTY     ' '
-#define CURSOR_PAIR   1
-#define TOKEN_PAIR    2
-#define EMPTY_PAIR    3
+#define EMPTY ' '
+#define CURSOR_PAIR 1
+#define TOKEN_PAIR 2
+#define EMPTY_PAIR 3
 #define LINES 11
 #define COLS 11
 #define TOKENS 5
 
+pthread_t token[TOKENS];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int id[TOKENS] = {0, 1, 2, 3, 4};
 int is_move_okay(int y, int x);
+void check_move();
 void draw_board(void);
-void move_token(int token);
+void *move_token(void *token);
 void move_tokens();
 void board_refresh(void);
 
 char board[LINES][COLS];
 
-typedef struct CoordStruct {
+typedef struct CoordStruct
+{
   int x;
   int y;
 } coord_type;
- 
+
 coord_type cursor, coord_tokens[TOKENS];
 
-int main(void) {
+int main(void)
+{
   int ch;
-  srand(time(NULL));  /* inicializa gerador de numeros aleatorios */ 
+  srand(time(NULL)); /* inicializa gerador de numeros aleatorios */
 
   /* inicializa curses */
 
@@ -38,14 +45,15 @@ int main(void) {
 
   /* inicializa colors */
 
-  if (has_colors() == FALSE) {
+  if (has_colors() == FALSE)
+  {
     endwin();
     printf("Seu terminal nao suporta cor\n");
     exit(1);
   }
 
-    start_color();
-    
+  start_color();
+
   /* inicializa pares caracter-fundo do caracter */
 
   init_pair(CURSOR_PAIR, COLOR_YELLOW, COLOR_YELLOW);
@@ -54,68 +62,118 @@ int main(void) {
   clear();
 
   draw_board(); /* inicializa tabuleiro */
-  
-  do {   
+
+  do
+  {
     move_tokens(); /* move os tokens aleatoriamente */
+    check_move();
     board_refresh(); /* redesenha tabuleiro */
-    
+
     ch = getch();
-    switch (ch) {
-      case KEY_UP:
-      case 'w':
-      case 'W':
-        if ((cursor.y > 0)) {
-          cursor.y = cursor.y - 1;
-        }
-        break;
-      case KEY_DOWN:
-      case 's':
-      case 'S':
-        if ((cursor.y < LINES - 1)) {
-          cursor.y = cursor.y + 1;
-        }
-        break;
-      case KEY_LEFT:
-      case 'a':
-      case 'A':
-        if ((cursor.x > 0)) {
-          cursor.x = cursor.x - 1;
-        }
-        break;
-      case KEY_RIGHT:
-      case 'd':
-      case 'D':
-        if ((cursor.x < COLS - 1)) {
-          cursor.x = cursor.x + 1;
-        }
-        break;
+    switch (ch)
+    {
+    case KEY_UP:
+    case 'w':
+    case 'W':
+      if ((cursor.y > 0))
+      {
+        cursor.y = cursor.y - 1;
+      }
+      break;
+    case KEY_DOWN:
+    case 's':
+    case 'S':
+      if ((cursor.y < LINES - 1))
+      {
+        cursor.y = cursor.y + 1;
+      }
+      break;
+    case KEY_LEFT:
+    case 'a':
+    case 'A':
+      if ((cursor.x > 0))
+      {
+        cursor.x = cursor.x - 1;
+      }
+      break;
+    case KEY_RIGHT:
+    case 'd':
+    case 'D':
+      if ((cursor.x < COLS - 1))
+      {
+        cursor.x = cursor.x + 1;
+      }
+      break;
     }
-  }while ((ch != 'q') && (ch != 'Q'));
+  } while ((ch != 'q') && (ch != 'Q'));
   endwin();
   exit(0);
 }
 
-void move_tokens() {
+void move_tokens()
+{
   int i;
   for (i = 0; i < TOKENS; i++)
-    move_token(i);
+  {
+    pthread_create(&token[i], NULL, move_token, (void *)&id[i]);
+  }
 }
 
-void board_refresh(void) {
+void *move_token(void *token)
+{
+  int i = *((int *)token), new_x, new_y;
+  /* determina novas posicoes (coordenadas) do token no tabuleiro (matriz) */
+
+  do
+  {
+    new_x = rand() % (COLS);
+    new_y = rand() % (LINES);
+  } while ((board[new_x][new_y] != 0) || ((new_x == cursor.x) && (new_y == cursor.y)));
+
+  /* retira token da posicao antiga  */
+
+  board[coord_tokens[i].x][coord_tokens[i].y] = 0;
+  board[new_x][new_y] = *((int *)token);
+
+  /* coloca token na nova posicao */
+  coord_tokens[i].x = new_x;
+  coord_tokens[i].y = new_y;
+}
+
+void check_move()
+{
+  int i;
+  for (i = 0; i < TOKENS; i++)
+  {
+    if (coord_tokens[i].x == cursor.x && coord_tokens[i].y == cursor.y)
+    {
+      board[coord_tokens[i].x][coord_tokens[i].y] = 0;
+    }
+  }
+}
+
+int is_move_okay(int x, int y)
+{
+}
+
+void board_refresh(void)
+{
   int x, y, i;
 
   /* redesenha tabuleiro "limpo" */
 
-  for (x = 0; x < COLS; x++) 
-    for (y = 0; y < LINES; y++){
+  for (x = 0; x < COLS; x++)
+    for (y = 0; y < LINES; y++)
+    {
       attron(COLOR_PAIR(EMPTY_PAIR));
       mvaddch(y, x, EMPTY);
       attroff(COLOR_PAIR(EMPTY_PAIR));
-  }
+    }
 
   /* poe os tokens no tabuleiro */
 
-  for (i = 0; i < TOKENS; i++) {
+  for (i = 0; i < TOKENS; i++)
+  {
     attron(COLOR_PAIR(TOKEN_PAIR));
     mvaddch(coord_tokens[i].y, coord_tokens[i].x, EMPTY);
     attroff(COLOR_PAIR(TOKEN_PAIR));
@@ -129,31 +187,12 @@ void board_refresh(void) {
   attroff(COLOR_PAIR(CURSOR_PAIR));
 }
 
-void move_token(int token) {
-  int i = token, new_x, new_y; 
-
-  /* determina novas posicoes (coordenadas) do token no tabuleiro (matriz) */
-
-  do{
-    new_x = rand()%(COLS);
-    new_y = rand()%(LINES);
-  } while((board[new_x][new_y] != 0) || ((new_x == cursor.x) && (new_y == cursor.y)));
- 
-  /* retira token da posicao antiga  */ 
-
-  board[coord_tokens[i].x][coord_tokens[i].y] = 0; 
-  board[new_x][new_y] = token; 
-
-  /* coloca token na nova posicao */ 
-  coord_tokens[i].x = new_x;
-  coord_tokens[i].y = new_y;
-}
-
-void draw_board(void) {
+void draw_board(void)
+{
   int x, y;
 
   /* limpa matriz que representa o tabuleiro */
-  for (x = 0; x < COLS; x++) 
-    for (y = 0; y < LINES; y++) 
+  for (x = 0; x < COLS; x++)
+    for (y = 0; y < LINES; y++)
       board[x][y] = 0;
 }
